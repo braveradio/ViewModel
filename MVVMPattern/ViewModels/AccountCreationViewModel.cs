dd0c4fbfbe109a1746114260ba556aec5735de33
Ильнур Гиззатуллин <juvebars@mail.ru>
2020-02-25 12:06:28 +0300

    button was added

--- MVVMPattern/ViewModels/AccountCreationViewModel.cs
+++ MVVMPattern/ViewModels/AccountCreationViewModel.cs
@@ -4,17 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MVVMPattern.ViewModels
 {
     public class AccountCreationViewModel
     {
         public AccountModel NewAccount { get; set; }
+        public string Message { get; set; } = "Fuck You!";
 
         public AccountCreationViewModel()
         {
             NewAccount = new AccountModel();
+
         }
+
+        private RelayCommand simpleCommand;
+        public RelayCommand SimpleCommand
+        {
+            get 
+            {
+                return simpleCommand ?? (simpleCommand = new RelayCommand(obj => MessageBox.Show(Message))); 
+            }
+        }
+
+
+        private void DoSimpleCommand()
+        {
+            MessageBox.Show("Fuck you!");
+        }
+
+
 
         public void ValidatePassword()
         {
